f283309968ae098abb2b2e172b6f5efb704e56c8
HarmonySwift <112459623+HarmonySwift@users.noreply.github.com>
2022-10-09 21:19:16 +0700

    Update slhostdns.sh

--- slowdns/slhostdns.sh
+++ slowdns/slhostdns.sh
@@ -9,11 +9,11 @@ rm nsdomain
 
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 subsl=$(</dev/urandom tr -dc a-x0-9 | head -c5)
-DOMAIN=mantapxsl.my.id
-SUB_DOMAIN=onichan-${sub}.mantapxsl.my.id
-NS_DOMAIN=slowdns-${subsl}.mantapxsl.my.id
-CF_ID=slinfinity69@gmail.com
-CF_KEY=dd2c5e0313f122b3c1833471d469b1025f492
+DOMAIN=harmonyswift.com
+SUB_DOMAIN=hf-${sub}.harmonyswift.com
+NS_DOMAIN=slowdns-${subsl}.harmonyswift.com
+CF_ID=auperwing@gmail.com
+e79431dc85570f38811c05f99bf44aae02501
 echo "IP=""$SUB_DOMAIN" >> /var/lib/crot/subdomain.conf
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
